4fdae288f21daf678c325333e57304c8c53e7a6a
redyf <mateusalvespereira7@gmail.com>
2024-11-02 12:27:15 -0300

    feat: add initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,19 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        oxoBackground: "var(--oxo-background)",
+        oxoForeground: "var(--oxo-foreground)",
+        oxoBlue: "#81a2be",
+        oxoRed: "#cc6666",
+        oxoGreen: "#b5bd68",
+        oxoYellow: "#f0c674",
+        oxoCyan: "#8abeb7",
+        oxoMagenta: "#b294bb",
       },
     },
   },
